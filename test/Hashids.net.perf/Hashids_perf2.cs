fe6267cac236513145f0317b9fc385d14f9bf436
Stephen Oberholtzer <stevie@qrpff.net>
2019-10-04 16:44:49 -0400

    Add memory usage benchmarking

--- test/Hashids.net.perf/Hashids_perf2.cs
+++ test/Hashids.net.perf/Hashids_perf2.cs
@@ -5,12 +5,14 @@ using BenchmarkDotNet.Attributes;
 
 namespace HashidsNet.perf
 {
+    [MemoryDiagnoser]
     public class Hashids_perf2
     {
         Hashids h;
 
         static Hashids_perf2()
         {
+            Hashids.ExploitArrayVariance = true;
         }
 
         public Hashids_perf2()
@@ -29,5 +31,12 @@ namespace HashidsNet.perf
         {
             return h.DecodeLong(h.EncodeLong(long.MaxValue))[0];
         }
+
+        [Benchmark]
+        public ulong RoundTripUInt64()
+        {
+            return h.DecodeUnsignedLong(h.EncodeUnsignedLong(ulong.MaxValue))[0];
+        }
     }
 }
+
