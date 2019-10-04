f2346a4a67c0aec6883a9ebfa2a693e5beb5bdd5
Stephen Oberholtzer <stevie@qrpff.net>
2019-10-04 16:19:13 -0400

    BenchmarkDotNet support

---
+++ test/Hashids.net.perf/Hashids_perf2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace HashidsNet.perf
+{
+    public class Hashids_perf2
+    {
+        Hashids h;
+
+        static Hashids_perf2()
+        {
+        }
+
+        public Hashids_perf2()
+        {
+            h = new Hashids();
+        }
+
+        [Benchmark]
+        public int RoundTripInt32()
+        {
+            return h.Decode(h.Encode(int.MaxValue))[0];
+        }
+
+        [Benchmark]
+        public long RoundTripInt64()
+        {
+            return h.DecodeLong(h.EncodeLong(long.MaxValue))[0];
+        }
+    }
+}
---
+++ test/Hashids.net.perf/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Hashids.net.perf
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
